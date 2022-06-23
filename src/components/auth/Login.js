8802f6fcece6dc045637c695d8ea14cd19e9d511
HetviSolanki49701 <hetvis.solanki@gmail.com>
2022-06-23 23:15:43 +0530

    backend folder and login images

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import { useState } from "react";
 // import {login} from "../api/auth";
 
 const Login = () => {
-  const [email, setEmail] = useState("sss@gmail.com");
-  const [password, setPassword] = useState("ss@3");
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   const handleSubmit = async (e) => {
     // e.preventDefault();
@@ -19,43 +19,53 @@ const Login = () => {
   return (
     <div className="container p-5">
       <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <form onSubmit={handleSubmit}>
-          <h2>Login</h2>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Your email"
-                autoFocus
-              />
-            </div>
+        <div className="col">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            className="w-sm"
+            alt="Sample image"
+          />
+        </div>
+        <div className="col">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Your email"
+                    autoFocus
+                  />
+                </div>
 
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Your password"
-              />
-            </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Your password"
+                  />
+                </div>
 
-            <br />
-            <button
-              onClick={handleSubmit}
-              type="primary"
-              className="mb-3 btn btn-primary"
-            >
-              Login with Email/Password
-            </button>
-          </form>
+                <br />
+                <button
+                  onClick={handleSubmit}
+                  type="primary"
+                  className="mb-3 btn btn-primary"
+                >
+                  Login with Email/Password
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    // <>dfdgdfgrg</>
   );
 };
 
