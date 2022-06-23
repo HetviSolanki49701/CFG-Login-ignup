71406e96b1aea9e9628c2c313d37b0a256d30655
srishtiwadhwa <71011538+srishtiwadhwa@users.noreply.github.com>
2022-06-23 14:13:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+export default App;
+import React, { Component } from 'react';
+import Login from './components/login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>CFG LOGIN SIGNUP</h1>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+       <h1>CFG LOGIN SIGNUP</h1>
+				<Login />
+			</div>
+		);
+	}
 }
 
 export default App;
+
